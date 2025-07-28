17e3786a212bfd9cc079e2781c30c6ff8b24e2b2
ot-coder <ogboanthony315@gmail.com>
2025-07-28 12:04:39 +0200

    messing around with functions

--- intro.hs
+++ intro.hs
@@ -1,7 +1,16 @@
 double x = x * 2
+b = tail [2,3,4,5,6]
+c = 5
+d = 8
+e x = d + x*2
+
 
 main = do
     putStrLn "Enter a number:"
     input <- getLine
     let number = read input :: Int
-    print (double number)
+    let doubled = double number
+    print doubled
+    print (b)
+    print (e doubled)
+    
